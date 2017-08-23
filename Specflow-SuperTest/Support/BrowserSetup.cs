5658cce236ea1265221440158859b52085e0112b
ayau1 <amie.yau@bglgroup.co.uk>
2017-08-23 09:48:38 +0100

    added new settings to app config

--- Specflow-SuperTest/Support/BrowserSetup.cs
+++ Specflow-SuperTest/Support/BrowserSetup.cs
@@ -11,27 +11,55 @@ namespace Specflow_SuperTest
         public static IWebDriver Driver;
         protected static string Username;
         protected static string AccessKey;
+        private string _localOrCI=System.Configuration.ConfigurationManager.AppSettings["localOrCI"];
+
+        private string _useLocalDriverOrSauceLabs =
+            System.Configuration.ConfigurationManager.AppSettings["useLocalDriverOrSaucelabs"]; 
 
 
         static BrowserSetup()
         {
+
+            //local with local webdriver
+
+            /*
+             * get the app config setting for where I want to run it (myComputer or CI)
+             * get the app config for how (local web or sauce)
+             * if app config set to myComputer
+             *   if I want to use saucelabs
+             *      then driver should be set to sauce remote driver with desired capabilities set from app config
+             *   if I want to run with local webdriver
+             *          then driver should be set to local webdriver
+             *  else if app config set to CI
+             *      then driver should be set to sauce remote driver with desired capabilities set from environment variables
+             */
+
             Username = Environment.GetEnvironmentVariable("SauceLabsUserName");
             AccessKey = Environment.GetEnvironmentVariable("SauceLabsAccessKey");
-            DesiredCapabilities caps = new DesiredCapabilities();
-            caps.SetCapability(CapabilityType.BrowserName,"Safari");
-            caps.SetCapability(CapabilityType.Version, "latest");
-            caps.SetCapability(CapabilityType.Platform, "iOS");
-            caps.SetCapability("deviceName", "iPhone 7 Simulator");
-            caps.SetCapability("deviceOrientation", "portrait");
-            caps.SetCapability("appiumVersion","1.6.4");
-            caps.SetCapability("username", Username);
-            caps.SetCapability("accessKey", AccessKey);
-            caps.SetCapability("name", TestContext.CurrentContext.Test.Name);
-            //Driver = new ChromeDriver();
-            Driver = new RemoteWebDriver(new Uri("http://ondemand.saucelabs.com:80/wd/hub"), caps,
-                TimeSpan.FromSeconds(600));
-                
-               
+
+        }
+
+        public void CreateNewWebDriver()
+        {
+            if (_localOrCI == "local")
+            {
+                if (_useLocalDriverOrSauceLabs == "localDriver")
+                {
+                    DesiredCapabilities caps = new DesiredCapabilities();
+                    caps.SetCapability(CapabilityType.BrowserName, System.Configuration.ConfigurationManager.AppSettings["BrowserName"]);
+                    caps.SetCapability(CapabilityType.Version, System.Configuration.ConfigurationManager.AppSettings["Version"]);
+                    caps.SetCapability(CapabilityType.Platform, System.Configuration.ConfigurationManager.AppSettings["Platform"]);
+                    caps.SetCapability("deviceName", System.Configuration.ConfigurationManager.AppSettings["deviceName"]);
+                    caps.SetCapability("deviceOrientation", System.Configuration.ConfigurationManager.AppSettings["deviceOrientation"]);
+                    caps.SetCapability("appiumVersion", System.Configuration.ConfigurationManager.AppSettings["appiumVersion"]);
+                    caps.SetCapability("username", Username);
+                    caps.SetCapability("accessKey", AccessKey);
+                    caps.SetCapability("name", TestContext.CurrentContext.Test.Name);
+
+                    Driver = new RemoteWebDriver(new Uri("http://ondemand.saucelabs.com:80/wd/hub"), caps,
+                        TimeSpan.FromSeconds(600));
+                }
+            }
         }
     }
 }
